90873ff1e8ade76e7585e16e4063dd1e324ceef9
Microhom <microhom@gmail.com>
2017-08-27 18:26:27 -0300

    stepper examples

---
+++ StepperControl/StepperControl.ino
@@ -0,0 +1,60 @@
+int Distance = 0;  // Grabar el numero de pasos que hemos dado
+
+// Define our maximum and minimum speed in steps per second (scale pot to these)
+#define RPMS 300.0
+#define STEP_PIN                2
+#define DIRECTION_PIN           3
+#define GO_PIN                  10
+
+#define STEPS_PER_REV         200
+#define MICROSTEPS_PER_STEP     8
+#define MICROSECONDS_PER_MICROSTEP   (1000000/(STEPS_PER_REV * MICROSTEPS_PER_STEP)/(RPMS / 60))
+
+
+uint32_t LastStepTime = 0;
+uint32_t CurrentTime = 0;
+
+
+void setup() {               
+  pinMode(STEP_PIN, OUTPUT);    
+  pinMode(DIRECTION_PIN, OUTPUT);
+  digitalWrite(STEP_PIN, LOW);
+  digitalWrite(DIRECTION_PIN, LOW);
+}
+
+void loop() {
+   if (digitalRead(GO_PIN) == LOW)
+  {
+    CurrentTime = micros();
+    if ((CurrentTime - LastStepTime) > MICROSECONDS_PER_MICROSTEP)
+    {
+  LastStepTime = CurrentTime;
+  digitalWrite(STEP_PIN, HIGH);
+  delayMicroseconds((MICROSECONDS_PER_MICROSTEP * 0.9)/2);       
+  digitalWrite(STEP_PIN, LOW);
+  delayMicroseconds((MICROSECONDS_PER_MICROSTEP * 0.9)/2);
+  Distance = Distance + 1;   // record this step
+ 
+  // Revisar si estamos al final de la secuencia de pasos
+  if (Distance == 1500)
+  {
+    // We are! Reverse direction (invert DIR signal)
+    if (digitalRead(DIRECTION_PIN) == LOW)
+    {
+      digitalWrite(DIRECTION_PIN, HIGH);
+    }
+    else
+    {
+      digitalWrite(DIRECTION_PIN, LOW);
+    }
+    // Reset our distance back to zero since we're
+    // starting a new move
+    Distance = 0;
+    // Now pause for half a second
+    delay(500);
+
+   
+  }
+}
+}
+}
---
+++ StepperDemo/StepperDemo.ino
@@ -0,0 +1,166 @@
+/****************************************************************************** 
+
+[HIGH/LOW functionality ]
+______________________________
+[MS1] - [MS2] - [RESOLUTION]
+[ L ] - [ L ] - [FULL STEP ]
+[ H ] - [ L ] - [HALF STEP ]
+[ L ] - [ H ] - [QUARTER STEP]
+[ H ] - [ H ] - [EIGHT STEP]
+
+******************************************************************************/
+//Librerias
+#include <AccelStepper.h>
+
+//Declare pin functions 
+#define stp 2 // Logic Input. transition from LOW to HIGH will trigger the motor to step forward one step
+#define dir 3 // Logic Input. This pin determines the direction of motor rotation
+#define MS1 4 // Logic Input. HIGH/LOW functionality
+#define MS2 5 // Logic Input. HIGH/LOW functionality
+#define EN  6 // Enables the FET functionality within the motor driver. Set to LOW to enable FETs allowing motor control
+
+
+//Declare variables for functions
+char user_input;
+int x;
+int y;
+int state;
+
+void setup() {
+  pinMode(stp, OUTPUT);
+  pinMode(dir, OUTPUT);
+  pinMode(MS1, OUTPUT);
+  pinMode(MS2, OUTPUT);
+  pinMode(EN, OUTPUT);        
+  resetEDPins(); //Set step, direction, microstep and enable pins to default states
+  Serial.begin(9600); //Open Serial connection for debugging
+  Serial.println("Iniciar control del motor");
+  Serial.println();
+  //Print function list for user selection
+  Serial.println("ingresa el numero en la terminal para controlar tu motor:");
+  Serial.println("1. Giro el motor en micropasos.");
+  Serial.println("2. Reversa en micropasos.");
+  Serial.println("3. Girar a 1/8 de micropasos.");
+  Serial.println("4. Paso completo hacia adelante y reversa.");
+  Serial.println();
+}
+
+//Main loop
+void loop() {
+  while(Serial.available()){
+      user_input = Serial.read(); //Read user input and trigger appropriate function
+      digitalWrite(EN, LOW); //Pull enable pin low to allow motor control
+      if (user_input =='1')
+      {
+         StepForwardDefault();
+      }
+      else if(user_input =='2')
+      {
+        ReverseStepDefault();
+      }
+      else if(user_input =='3')
+      {
+        SmallStepMode();
+      }
+      else if(user_input =='4')
+      {
+        ForwardBackwardStep();
+      }
+      else
+      {
+        Serial.println("Ingresa una nueva orden");
+      }
+      resetEDPins();
+  }
+}
+
+//Reset Easy Driver pins to default states
+void resetEDPins()
+{
+  digitalWrite(stp, LOW);
+  digitalWrite(dir, LOW);
+  digitalWrite(MS1, HIGH);
+  digitalWrite(MS2, HIGH);
+  digitalWrite(EN, HIGH);
+}
+
+//Default microstep mode function
+void StepForwardDefault()
+{
+  Serial.println(">> [El motor se mueve hacia adelante]");
+  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
+  for(x= 1; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
+  {
+    digitalWrite(stp,HIGH); //Trigger one step forward
+    delay(1);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delay(1);
+  }
+  Serial.println(" _ Prueba una nueva opcion _");
+  Serial.println();
+}
+
+//Reverse default microstep mode function
+void ReverseStepDefault()
+{
+  Serial.println(">> [El motor gira en reversa]");
+  digitalWrite(dir, HIGH); //Pull direction pin high to move in "reverse"
+  for(x= 1; x<1000; x++)  //Loop the stepping enough times for motion to be visible
+  {
+    digitalWrite(stp,HIGH); //Trigger one step
+    delay(1);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delay(1);
+  }
+  Serial.println("_ Prueba una nueva opcion _");
+  Serial.println();
+}
+
+// 1/8th microstep foward mode function
+void SmallStepMode()
+{
+  Serial.println("Stepping at 1/8th microstep mode.");
+  digitalWrite(dir, LOW); //Pull direction pin low to move "forward"
+  digitalWrite(MS1, HIGH); //Pull MS1, and MS2 high to set logic to 1/8th microstep resolution
+  digitalWrite(MS2, HIGH);
+  for(x= 1; x<1000; x++)  //Loop the forward stepping enough times for motion to be visible
+  {
+    digitalWrite(stp,HIGH); //Trigger one step forward
+    delay(1);
+    digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+    delay(1);
+  }
+  Serial.println("Enter new option");
+  Serial.println();
+}
+
+//Forward/reverse stepping function
+void ForwardBackwardStep()
+{
+  Serial.println("Alternate between stepping forward and reverse.");
+  for(x= 1; x<5; x++)  //Loop the forward stepping enough times for motion to be visible
+  {
+    //Read direction pin state and change it
+    state=digitalRead(dir);
+    if(state == HIGH)
+    {
+      digitalWrite(dir, LOW); // adelante      dir.runSpeed();
+    
+    }
+    else if(state ==LOW)
+    {
+      digitalWrite(dir,HIGH); // atras
+   
+    }
+    
+    for(y=1; y<1000; y++)
+    {
+      digitalWrite(stp,HIGH); //Trigger one step
+      delay(1);
+      digitalWrite(stp,LOW); //Pull step pin low so it can be triggered again
+      delay(1);
+    }
+  }
+  Serial.println("Enter new option:");
+  Serial.println();
+}
